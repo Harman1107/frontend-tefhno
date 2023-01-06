7f33b3a66eea584ec778fae3b9ad38c31db3987b
Harman1107 <2020kuec2001@iiitkota.ac.in>
2023-01-06 14:00:16 +0530

    init

---
+++ src/components/Columns.tsx
@@ -0,0 +1,19 @@
+export const COLUMNS = [
+    {
+        Header:'Name',
+        accessor:'firstName'
+
+    },
+    {
+        Header:'Status',
+        accessor:'ssn'
+    },
+    {
+        Header:'Role',
+        accessor:'ein'
+    },
+    {
+        Header:'Last Login',
+        accessor:'ip'
+    }
+]
